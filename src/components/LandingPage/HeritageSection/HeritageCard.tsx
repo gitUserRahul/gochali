2c19b23a9a178e3b59ffd6131184e8f8f01d773f
NarayanChaudhary <chaudharynarayan799@gmail.com>
2026-04-24 11:16:26 +0545

    [FIXED]: design_fixes :: gallery folder/file renamed to heritage

--- src/components/LandingPage/HeritageSection/HeritageCard.tsx
+++ src/components/LandingPage/HeritageSection/HeritageCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 type GalleryCardProps ={
   category: string;
@@ -16,16 +17,18 @@ const GalleryCard = ({ category, title, image }: GalleryCardProps ) => {
           <Image src={image} alt={title} fill className=" object-cover" />
         </div>
 
-        <div className="absolute z-50 max-w-80.5  w-full bg-white px-7.5 py-4 rounded-[12px] left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Link href="" className="group absolute z-50 max-w-80.5 w-full bg-white px-[23px] py-[16px] rounded-[12px] left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between items-end">
           <div className="flex flex-col gap-2.5">
-            <Link href="" className="inline-flex">
-              <span className="px-3 py-1 rounded bg-primary-color text-white font-medium text-[14px]">
+              <span className="px-3 py-1 rounded bg-primary-color text-white font-medium text-[14px] w-fit">
                 {category}
               </span>
-            </Link>
             <p className="font-semibold text-lg text-[#292929]">{title}</p>
           </div>
-        </div>
+          <div className="bg-[#FFE1DE] w-11 h-11 flex items-center justify-center rounded-[30px] transition-all duration-300 ease-in-out  group-hover:translate-x-2">
+          <FaArrowRightLong
+              className="w-4 h-4 text-primary-color"/>
+          </div>
+        </Link>
       </div>
     </div>
   );
